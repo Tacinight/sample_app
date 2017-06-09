2f50d3d369fb0d6d4f16acd63990043b85f05951
Evan Zhao <tacingiht@gmail.com>
2017-06-09 10:31:46 +0800

    Add basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new 
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def create 
+  def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user 
-    else 
+      log_in @user
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
       render 'new'
-    end 
-  end 
+    end
+  end
 
   private
-  def user_params 
+
+  def user_params
     params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-  end 
+                                 :password_confirmation)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,67 +1,65 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
   def setup
-    @user = User.new(name: "Evan Zhao", email: "tacinight@outlook.com",
-                     password: "foobar", password_confirmation: "foobar")
-  end 
-  
-  test "the truth" do
+    @user = User.new(name: 'Evan Zhao', email: 'tacinight@outlook.com',
+                     password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  test 'the truth' do
     assert @user.valid?
   end
 
-  test "name should be present" do 
-    @user.name = "    "
+  test 'name should be present' do
+    @user.name = '    '
     assert_not @user.valid?
-  end 
+  end
 
-  test "email should be present" do
-    @user.email = "    "
+  test 'email should be present' do
+    @user.email = '    '
     assert_not @user.valid?
-  end 
+  end
 
-  test "name should not be too long" do
+  test 'name should not be too long' do
     @user.name = 'a' * 51
     assert_not @user.valid?
-  end 
+  end
 
-  test "email should not be too long" do
-    @user.email = 'a' * 244 + "@example.com"
+  test 'email should not be too long' do
+    @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
-  end 
+  end
 
-  test "email validation should accept valid addresses" do 
+  test 'email validation should accept valid addresses' do
     valid_addresses = %w[user@example.com USERE@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
-    end 
-  end 
+    end
+  end
 
-  test "email address should be unique" do
-    duplicate_user = @user.dup 
+  test 'email address should be unique' do
+    duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
-  end 
+  end
 
-  test "email addresses should be saved as lower-case" do 
-    mixed_case_email = "Foo@ExAMple.coM"
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMple.coM'
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
-  end 
+  end
 
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
     assert_not @user.valid?
-  end 
+  end
 
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
-  end 
-  
+  end
 end
